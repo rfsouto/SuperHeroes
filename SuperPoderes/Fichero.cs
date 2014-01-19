6a33cf093e254eee39b2f5f67f2a0b3a68f4f28e
rfsouto <rfernandezsouto@gmail.com>
2014-01-19 19:20:17 +0100

    Commit con comparativas

--- SuperPoderes/Fichero.cs
+++ SuperPoderes/Fichero.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Configuration;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
 
 namespace SuperPoderes
 {
@@ -12,16 +15,47 @@ namespace SuperPoderes
     {
         private static readonly Lazy<SPFile> _file = new Lazy<SPFile>(() => new SPFile());
         private List<String> roles = new List<string>();
+        private String cadRegExp;
 
         private SPFile()
         {
+            Stopwatch timer = new Stopwatch();
             //Inicializamos el objeto fichero
             openData();
             //Realizamos la lectura del mismo y realizamos la separación por los condicionantes
-            separateRoles();
+            
+            //Ejemplo con Linq
+            timer.Start();
+            separateRolesLinq();
+            timer.Stop();
+            Console.WriteLine("Tiempo de ejecución con Linq: {0}", timer.Elapsed.ToString());
+            
+            //Ejemplo bucle for
+            timer.Restart();
+            separateRolesFor();
+            timer.Stop();
+            Console.WriteLine("Tiempo de ejecución con un bucle for: {0}", timer.Elapsed.ToString());
+
+            //Ejemplo bucle foreach
+            timer.Restart();
+            separateRolesForEach();
+            timer.Stop();
+            Console.WriteLine("Tiempo de ejecución con un bucle foreach: {0}", timer.Elapsed.ToString());
+            
+            //Ejemplo Expresiones regulares
+
+            //roles.All(x => { x = "," + x; return true; });
+            //myCollection = myCollection.Select(m => m.Substring(0, 8)).ToArray();
+
+            //cadRegExp = string.Join(",", roles);
+            //timer.Restart();
+            //separateRolesRegularExp();
+            //timer.Stop();
+            //Console.WriteLine("Tiempo de ejecución con expresiones regulares: {0}", timer.Elapsed.ToString());
+
+            Console.WriteLine("Pulsa una tecla para finalizar.");
             Console.ReadKey();
         }
-
 
         public static SPFile instancia
         {
@@ -34,32 +68,21 @@ namespace SuperPoderes
         private void openData()
         {
             String filePath;
-
             filePath = System.Configuration.ConfigurationManager.AppSettings["filePath"];
-
             if (existsFilePath(filePath))
             {
-                string line;
-                System.IO.StreamReader file = new System.IO.StreamReader(filePath);
-                while ((line = file.ReadLine()) != null)
-                {
-                    roles.Add(line);
-                }
-                file.Close();
+                roles = System.IO.File.ReadAllLines(filePath).ToList();
             }
             else
             {
-                System.Console.WriteLine("File path doesn´t exists.");
-                System.Console.ReadKey();
+                Console.WriteLine("La ruta configurada no existe: {0}. ", filePath);
             }
-
         }
 
         private void generateDat(string outPath, List<String> names)
         {
             System.IO.File.WriteAllLines(outPath, names);
         }
-
 
         private Boolean existsFilePath(string path)
         {
@@ -73,15 +96,71 @@ namespace SuperPoderes
          * -> Apunte, sobreentiendo que se habla de "D" y de "d", si fuera sólo D, bastaría con quitar el ToUpper de la 
          * expresión lambda.
          */
-        private void separateRoles()
+        private void separateRolesLinq()
         {
                 //Si en el futuro queremos cambiar la condición de separación sólo tenemos que modificar esta función.             
                 //Extraemos los villanos
                 List<String> villains = roles.Where(x => x.ToUpper().Contains('D')).ToList();
                 generateDat("Villanos.dat", villains);
-                //Extraemos los héroes en base a los villanos encontrados. 
-                List<String> heroes = roles.Where(x => !villains.Contains(x)).ToList();
-                generateDat("SuperHeroes.dat", heroes);
         }
+
+        private void separateRolesFor()
+        {           
+            List<String> villains = new List<string>();
+            List<String> heroes = new List<string>();
+            //Extraemos los villanos
+            for (int i = 0; i < roles.Count; i++)
+            {
+                if (roles[i].ToUpper().Contains("D"))
+                {
+                    villains.Add(roles[i]);
+                }
+            }
+            generateDat("VillanosFor.dat", villains);
+        }
+
+        private void separateRolesForEach()
+        {
+            List<String> villains = new List<string>();
+            List<String> heroes = new List<string>();
+            //Extraemos los villanos
+            foreach (string name in roles)
+                if (name.ToUpper().Contains("D"))
+                {
+                    villains.Add(name);
+                }
+
+            generateDat("VillanosForEach.dat", villains);
+         }
+
+        //private void separateRolesRegularExp()
+        //{
+        //    string pattern = @",*d*,";
+        //    List<String> villains = new List<string>();
+        //    List<String> heroes = new List<string>();
+        //    Match match;
+
+        //    Regex intAll = new Regex(pattern, RegexOptions.IgnoreCase);
+        //    match = intAll.Match(cadRegExp);
+        //    int matches = 0;
+        //    while (match.Success)
+        //    {
+        //        matches++;
+        //        // Do nothing with the match except get the next match.
+        //        match = match.NextMatch();
+        //    }
+
+        //    //foreach (string name in roles)
+        //    //{
+        //    //    if (Regex.IsMatch(name, pattern))
+        //    //    {
+        //    //        villains.Add(name);
+        //    //    }
+        //    //    else
+        //    //    {
+        //    //        heroes.Add(name);
+        //    //    }  
+        //    //}  
+        //}
     }
 }
